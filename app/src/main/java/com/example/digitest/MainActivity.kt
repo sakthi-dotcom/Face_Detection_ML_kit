1210d2b20afd99b5e752f1478026293d4cd83c4c
Sakthivel <sakthiveljeevaa@gmail.com>
2025-01-17 14:29:16 +0530

    Permission added

--- app/src/main/java/com/example/digitest/MainActivity.kt
+++ app/src/main/java/com/example/digitest/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.digitest
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import com.example.digitest.view.CameraScreen
 import com.example.digitest.viewmodel.CameraViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,12 +19,40 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val cameraViewModel: CameraViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CameraScreen(viewModel = cameraViewModel)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PermissionChecker.PERMISSION_GRANTED) {
+            setContent {
+                CameraScreen(viewModel = cameraViewModel)
+            }
+        } else {
+            showPermissionDeniedDialog()
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PermissionChecker.PERMISSION_GRANTED) {
+            setContent {
+                CameraScreen(viewModel = cameraViewModel)
+            }
+        }
+    }
+
+    private fun showPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Camera Permission Needed")
+            .setMessage("This app requires camera permission to function. Please enable it in the settings.")
+            .setPositiveButton("Go to Settings") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
 }
-
-
